94c8cad71d3dd1e7683e1db7e81efdbc762360c1
Manjinder Singh Rooprai <rooprai0044@gmail.com>
2024-03-21 23:12:53 -0700

    Angular initial project

--- Angular-initial/src/app/app.component.spec.ts
+++ Angular-initial/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-simple-example' title`, () => {
+  it(`should have the 'Angular-initial' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-simple-example');
+    expect(app.title).toEqual('Angular-initial');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-simple-example');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-initial');
   });
 });

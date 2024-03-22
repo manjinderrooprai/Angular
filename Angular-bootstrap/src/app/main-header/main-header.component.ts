fc0051a8d4f195476e27e475bab830abb361da49
Manjinder Singh Rooprai <rooprai0044@gmail.com>
2024-03-22 01:08:31 -0700

    Configure bootstrap

---
+++ Angular-bootstrap/src/app/main-header/main-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-header.component.html',
+  styleUrl: './main-header.component.css'
+})
+export class MainHeaderComponent {
+  headerName ="Employee Deatils"
+
+}
